b1f5b6bafd239ce05472e860cd6f0618adbbac6a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-18 17:02:18 -0500

    sources data from gdrive as well

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,11 +2,12 @@ const { google } = require('googleapis');
 const path = require(`path`);
 const mkdirp = require(`mkdirp`);
 const fs = require(`fs`);
+const lodash = require('lodash')
 
 const log = str => console.log(`\n🚗 `, str);
 const FOLDER = `application/vnd.google-apps.folder`;
 const GOOGLE_DOC = 'application/vnd.google-apps.document';
-
+const exportMime = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
 const defaultOptions = {
     folderId: '16donbs7-81ncyDK2G4XFI9b5Cf_I0GDD',
     key: '',
@@ -15,16 +16,29 @@ const defaultOptions = {
     ]
 };
 
-const getAuth = (options) => {
-    if (options.key) return JSON.parse(options.key);
-    if (fs.existsSync(options.pemFilePath)) {
-        return require(options.pemFilePath);
-    }
-}
-
-const getFolder = async (driveClient, folderId) => {
-    const { data: { files }} = await driveClient.files.list({ q: `'${folderId}' in parents`});
+const getFolder = async (gDriveClient, folderId) => {
+    const { data: { files }} = await gDriveClient.files.list({ q: `'${folderId}' in parents`});
     return files;
+};
+
+const getAuthorziedGdriveClient = (options) => {
+  let key;
+  const { scopes } = { ...options, ...defaultOptions };
+
+  if (options.key) key = JSON.parse(options.key);
+  if (fs.existsSync(options.pemFilePath)) {
+      key = require(options.pemFilePath);
+  }
+    // setting the general auth property for client
+    const token = new google.auth.JWT(
+      key.client_email,
+      null,
+      key.private_key,
+      scopes
+  );
+  google.options({ auth: token });
+
+  return google.drive('v3');
 };
 
 exports.onPreBootstrap = (
@@ -32,26 +46,14 @@ exports.onPreBootstrap = (
   options
 ) => {
   return new Promise(async (resolve) => {
-    log(`Started downloading content`);
+    log(`Started downloading content...`);
 
     // Get token and fetch root folder.
-    const key = getAuth(options);
-    const { createNode } = actions;
-    const { folderId, destination, scopes } = { ...options, ...defaultOptions };
-    
-    // setting the general auth property for client
-    const token = new google.auth.JWT(
-        key.client_email,
-        null,
-        key.private_key,
-        scopes
-    );
-
-    google.options({ auth: token });
+    const { folderId, destination } = { ...options, ...defaultOptions };
 
     // getting the drive client
-    const driveClient = google.drive('v3');
-    const filesInFolder = await getFolder(driveClient, folderId);
+    const gDriveClient = getAuthorziedGdriveClient(options);
+    const filesInFolder = await getFolder(gDriveClient, folderId);
 
     // Create content directory if it doesn't exist.
     mkdirp(destination);
@@ -59,7 +61,7 @@ exports.onPreBootstrap = (
     // Start downloading recursively through all folders.
     console.time(`Downloading content ⏲`);
 
-    Promise.all(fetchFilesInFolder(filesInFolder, undefined, driveClient, destination))
+    Promise.all(fetchFilesInFolder(filesInFolder, undefined, gDriveClient, destination))
       .then(() => {
         resolve();
         log(`Downloaded all files from Google Drive! 🍻`);
@@ -68,38 +70,44 @@ exports.onPreBootstrap = (
   });
 };
 
-function fetchFilesInFolder(filesInFolder, parent = '', driveClient, destination) {
+function fetchFilesInFolder(filesInFolder, parent = '', gDriveClient, destination, write = true) {
     const promises = [];
 
     filesInFolder.forEach(async (file) => {
       if (file.mimeType === FOLDER) {
         // If it`s a folder, create it in filesystem
         const snakeCasedFolderName = file.name.toLowerCase().split(' ').join('_');
-        log(`Creating folder ${parent}/${snakeCasedFolderName}`);
-        mkdirp(path.join(destination, parent, snakeCasedFolderName));
+        if (write) {
+            log(`Creating folder ${parent}/${snakeCasedFolderName}`);
+            mkdirp(path.join(destination, parent, snakeCasedFolderName));
+        }
 
         // Then, get the files inside and run the function again.
-        const nestedFiles = getFolder(driveClient, file.id)
+        const nestedFiles = getFolder(gDriveClient, file.id)
           .then((files) => {
             // combining array of promises into one.
-            return Promise.all(fetchFilesInFolder(files, `${parent}/${snakeCasedFolderName}`, driveClient, destination));
+            return Promise.all(fetchFilesInFolder(files, `${parent}/${snakeCasedFolderName}`, gDriveClient, destination, write));
           });
         promises.push(nestedFiles);
       }
       else {
         promises.push(
           new Promise(async (resolve, reject) => {
-            // If it`s a file, download it and convert to buffer.
-            const filePath = path.join(destination, parent, getFilenameByMime(file));
-            const { data } = await driveClient.files.get({ fileId: file.id, alt: 'media', fields: "*" }, { responseType: 'arraybuffer' });
-            const buff = new Buffer.from(data);
-            
-            fs.writeFile(filePath, buff, () => {
-              log(`${file.name} written`);
-              resolve(getFilenameByMime(file));
-            });
-          })
-        );
+            if (write) {
+                // If it`s a file, download it and convert to buffer.
+                const filePath = path.join(destination, parent, getFilenameByMime(file));
+                const driveResponse = await gDriveClient.files.get({ fileId: file.id, alt: 'media', fields: "*" }, { responseType: 'arraybuffer' });
+                const buff = new Buffer.from(driveResponse.data);
+                fs.writeFile(filePath, buff, () => {
+                    log(`${file.name} written`);
+                    return resolve(getFilenameByMime(file));
+                });
+            }
+            else {
+                const { data } = await gDriveClient.files.get({ fileId: file.id, fields: "description, name, kind, modifiedTime, trashed, id" });
+                resolve(data);
+            }
+        }));
       }
     });
 
@@ -124,3 +132,28 @@ const getFilenameByMime = file => {
     return file.name;
   }
 }
+
+exports.sourceNodes = async ({ actions }, options = defaultOptions) => {
+    log('creating graphql nodes...');
+    const { createNode } = actions;
+    const { folderId } = options;
+    const gDriveClient = getAuthorziedGdriveClient(options);
+  
+    const filesInFolder = await getFolder(gDriveClient, folderId);
+  
+    Promise.all(fetchFilesInFolder(filesInFolder, undefined, gDriveClient, '', false))
+      .then((allFiles) => {
+        lodash.flattenDeep(allFiles)
+            .filter((file) => !file.trashed)
+            .map((file) => ({
+                id: file.id,
+                description: file.description ? file.description : '',
+                name: file.name,
+                internal: {
+                    contentDigest: `${file.id}_${file.modifiedTime}`,
+                    type: 'gDriveContent'
+                }
+            }))
+            .forEach((file) => createNode(file));
+      });
+};
